63d4e7e40355a5b405700d6c979bc01756604d47
ritik6559 <ritikjoshi741@gmail.com>
2025-06-16 11:35:15 +0530

    adding meetings data table

--- src/modules/agents/ui/view/agents-view.tsx
+++ src/modules/agents/ui/view/agents-view.tsx
@@ -5,7 +5,7 @@ import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery} from "@tanstack/react-query";
 import LoadingState from "@/components/loading-state";
 import ErrorState from "@/components/error-state";
-import {DataTable} from "@/modules/agents/ui/components/data-table";
+import {DataTable} from "@/components/data-table";
 import {columns} from "@/modules/agents/ui/components/column";
 import EmptyState from "@/components/empty-state";
 import { useAgentsFilters } from '../../hooks/use-agents-filters';
--- src/modules/meetings/ui/view/meetings-view.tsx
+++ src/modules/meetings/ui/view/meetings-view.tsx
@@ -5,6 +5,9 @@ import {useTRPC} from "@/trpc/client";
 import {useSuspenseQuery} from "@tanstack/react-query";
 import LoadingState from "@/components/loading-state";
 import ErrorState from "@/components/error-state";
+import {DataTable} from "@/components/data-table";
+import {columns} from "@/modules/meetings/ui/components/columns";
+import EmptyState from "@/components/empty-state";
 
 const MeetingsView = () => {
 
@@ -12,8 +15,14 @@ const MeetingsView = () => {
     const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}))
 
     return (
-        <div>
-            {JSON.stringify(data)}
+        <div className={"flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4"} >
+            <DataTable columns={columns} data={data.items} />
+            {
+                data.items.length === 0 && <EmptyState
+                    title={"Create your first meeting"}
+                    description={"Schedule a meeting to connect with others."}
+                />
+            }
         </div>
     );
 };
